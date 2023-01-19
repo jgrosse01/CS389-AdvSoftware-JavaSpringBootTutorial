c8e8dac631e677e9c018bc242dcce0293cd562d7
jgrosse01 <jgrosse@carroll.edu>
2023-01-18 17:30:47 -0700

    ops: transfered basic functionality from HelloController and test to IndexController and test

--- src/main/java/edu/carroll/cs389/web/controller/IndexController.java
+++ src/main/java/edu/carroll/cs389/web/controller/IndexController.java
@@ -1,2 +1,12 @@
-package edu.carroll.cs389;public class IndexController {
+package edu.carroll.cs389.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
