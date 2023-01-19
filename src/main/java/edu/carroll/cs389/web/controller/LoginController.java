18b1999bab1cd8da6a58213561db7a1d8c0194c7
jgrosse01 <jgrosse@carroll.edu>
2023-01-18 19:56:33 -0700

    feat: displays global error for attempting to login without the hardcoded credentials

--- src/main/java/edu/carroll/cs389/web/controller/LoginController.java
+++ src/main/java/edu/carroll/cs389/web/controller/LoginController.java
@@ -1,14 +1,23 @@
 package edu.carroll.cs389.web.controller;
 
 import edu.carroll.cs389.web.form.LoginForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
+    // NOTE: in the tutorial it is specifically noted that if anything of this sort is used in an application our professor will hunt us down.
+    // I wouldn't put it past him and I do not intend to ever use any method like this for authentication.
+    private static final String validUser = "cs389user";
+    private static final String validPass = "supersecret";
+
     @GetMapping("/login")
     public String loginGet(Model model) {
         model.addAttribute("loginForm", new LoginForm());
@@ -16,13 +25,26 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginPost(@ModelAttribute LoginForm loginForm) {
+    public String loginPost(@Valid @ModelAttribute LoginForm loginForm, BindingResult result, RedirectAttributes attributes) {
         System.out.println("User '" + loginForm.getUsername() + "' attempted login");
+        if (result.hasErrors()) {
+            return "login";
+        }
+        // Username does not have to case match, password is exact
+        // NOTE: another interjection from the professor to similar affect as the last stating that password validation
+        // should never be done by string equality. this is for the purpose of example
+        if (!(validUser.equalsIgnoreCase(loginForm.getUsername()) &&
+                validPass.equals(loginForm.getPassword()))) {
+            result.addError(new ObjectError("globalError", "Username and password do not match any known users."));
+            return "login";
+        }
+        attributes.addAttribute("username", loginForm.getUsername());
         return "redirect:/loginSuccess";
     }
 
     @GetMapping("/loginSuccess")
-    public String loginSuccess() {
+    public String loginSuccess(String username, Model model) {
+        model.addAttribute("username", username);
         return "loginSuccess";
     }
 
