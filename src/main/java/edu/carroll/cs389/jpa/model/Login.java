e40da2471b27d8a0a62fe17fa1c1ebed7e0b4a72
jgrosse01 <jgrosse@carroll.edu>
2023-01-18 20:23:20 -0700

    ops: added database model for JPA H2 database

--- src/main/java/edu/carroll/cs389/jpa/model/Login.java
+++ src/main/java/edu/carroll/cs389/jpa/model/Login.java
@@ -1,2 +1,75 @@
-package edu.carroll.cs389.jpa.model;public class Login {
+package edu.carroll.cs389.jpa.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Table(name = "login")
+public class Login {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String hashedPassword;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    private static final String EOL = System.lineSeparator();
+    private static final String TAB = "\t";
+
+    @Override
+    public String toString() {
+        return "Login @ " + super.toString() + "[" + EOL +
+                TAB + "username=" + username + EOL +
+                TAB + "hashedPassword=" + "****" + EOL +
+                "]" + EOL;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+
+        final Login login = (Login)o;
+        return username.equals(login.username) && hashedPassword.equals(login.hashedPassword);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, hashedPassword);
+    }
 }
