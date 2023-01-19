ac5303099e3225f8281ae7f5aeac4a7f20917fe1
jgrosse01 <jgrosse@carroll.edu>
2023-01-18 20:51:41 -0700

    ops: created interface extended from JPA Repo that will search login table by username

--- src/main/java/edu/carroll/cs389/jpa/repo/LoginRepository.java
+++ src/main/java/edu/carroll/cs389/jpa/repo/LoginRepository.java
@@ -1,2 +1,11 @@
-package edu.carroll.cs389.jpa.repo;public class LoginRepository {
+package edu.carroll.cs389.jpa.repo;
+
+import java.util.List;
+
+import edu.carroll.cs389.jpa.model.Login;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoginRepository extends JpaRepository<Login, Integer> {
+    // JPA errors for returning a single object that doesn't exist so return a list despite it being empty or one item
+    List<Login> findByUsernameIgnoreCase(String username);
 }
