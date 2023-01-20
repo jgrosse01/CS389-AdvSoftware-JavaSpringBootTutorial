593936fb6e9b52bc4ef1bbbcece03be8084bba69
jgrosse01 <jgrosse@carroll.edu>
2023-01-19 18:24:54 -0700

    feat: LoginServices now log attempted logins and related info

--- src/main/java/edu/carroll/cs389/service/LoginServiceImpl.java
+++ src/main/java/edu/carroll/cs389/service/LoginServiceImpl.java
@@ -5,10 +5,15 @@ import edu.carroll.cs389.jpa.repo.LoginRepository;
 import edu.carroll.cs389.web.form.LoginForm;
 import org.springframework.stereotype.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 @Service
 public class LoginServiceImpl implements LoginService {
+    // static logger for the class
+    private static final Logger log = LoggerFactory.getLogger(LoginServiceImpl.class);
     private final LoginRepository loginRepository;
 
     public LoginServiceImpl(LoginRepository loginrepository) {
@@ -23,21 +28,27 @@ public class LoginServiceImpl implements LoginService {
      */
     @Override
     public boolean validateUser(LoginForm loginForm) {
+        // logging login info
+        log.info("validateUser: user '{}' attempted login", loginForm.getUsername());
         // case-insensitive lookup
         List<Login> users = loginRepository.findByUsernameIgnoreCase(loginForm.getUsername());
 
         // if we get zero or more than 1 user it is an error we do not deal with
-        if (users.size() != 1)
+        if (users.size() != 1) {
+            log.debug("validateUser: login failed, found {} users", users.size());
             return false;
+        }
 
         Login user = users.get(0);
         // There is a note here about NEVER including code that uses Java's hashcode in production and that has been noted.
         final String userProvidedHash = Integer.toString(loginForm.getPassword().hashCode());
         if (!user.getHashedPassword().equals(userProvidedHash)) {
+            log.debug("validateUser: password !match");
             return false;
         }
 
         // user exists to login and the provided password matches the one on file
+        log.debug("validateUser: successful login");
         return true;
     }
 }
